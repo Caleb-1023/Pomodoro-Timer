0f71ba7f1d8b963efcbd0966b29830a8f79b8f4e
John <johnudoumoh63@gmail.com>
2022-09-01 09:31:39 +0100

    chakra ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useTimer } from 'react-timer-hook';
+import { Box } from '@chakra-ui/react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const MyTimer = ({expiriyTimestamp}) => {
+  const { seconds,minutes,isRunning,start,pause} = useTimer({expiriyTimestamp, onExpire: () => console.warn('onExpire called')});
+  return(
+    <>
+    <div style={{ fontSize: '100px' }}>
+      <span>{minutes}</span>:<span>{seconds}</span>
     </div>
-  );
+    <p>{isRunning ? 'Running' : 'Not running'}</p>
+      <button onClick={start}>Start</button>
+      <button onClick={pause}>Stop</button>
+    </>
+  )
 }
+
+const App = () => {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
+  return (
+    <div>
+      <MyTimer expiryTimestamp={time} />
+    </div>
+  )
+}
+
+
 
 export default App;
