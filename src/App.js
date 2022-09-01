694c7cffa4d19487dc9265d8983e2bd9d052cbe8
John <johnudoumoh63@gmail.com>
2022-09-01 15:25:59 +0100

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useTimer } from 'react-timer-hook';
-import { Button } from '@chakra-ui/react'
-import { Box } from '@chakra-ui/react'
+import { Button, Container, Flex } from '@chakra-ui/react'
 
 const MyTimer = ({expiriyTimestamp}) => {
   const { seconds,minutes,isRunning,start,pause,restart} = useTimer({expiriyTimestamp, onExpire: () => console.warn('onExpire called')});
@@ -11,13 +10,15 @@ const MyTimer = ({expiriyTimestamp}) => {
       <span>{minutes}</span>:<span>{seconds}</span>
     </div>
     <p>{isRunning ? 'Running' : 'Not running'}</p>
-      <Button onClick={start}>Start</Button>
-      <Button onClick={pause}>Stop</Button>
-      <Button onClick={() => {
+    <Flex gap='3'> 
+      <Button colorScheme='green' onClick={start}>Start</Button>
+      <Button colorScheme='red' onClick={pause}>Stop</Button>
+      <Button colorScheme='blue' onClick={() => {
         const time = new Date()
         time.setSeconds(time.getSeconds() + 300);
         restart(time)
       }}>Restart</Button>
+    </Flex>
     </>
   )
 }
@@ -26,9 +27,9 @@ const App = () => {
   const time = new Date();
   time.setSeconds(time.getSeconds() + 600); // 10 minutes timer
   return (
-    <Box>
+    <Container centerContent = 'true'>
       <MyTimer expiryTimestamp={time} />
-    </Box>
+    </Container>
   )
 }
 
