61cd64d49f84b22452020a211b0f9e7d0da0f0b3
John <johnudoumoh63@gmail.com>
2022-09-02 01:10:38 +0100

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
-import { Container, Tabs, Tab, TabList, TabPanels, TabPanel } from "@chakra-ui/react";
+import React, {useState} from "react";
+import { Container, Tabs, Tab, TabList, TabPanels, TabPanel, Box, Center, useColorModeValue } from "@chakra-ui/react";
 import {Pomodoro, ShortBreak, LongBreak} from "./components/Timers/Timers";
-// import ShortBreak from "./components/ShortBreak/ShortBreak";
-
-const MyTimer = () => {
+import {Helmet} from 'react-helmet'
+const MyTimer = ({bg, setTabIndex}) => {
+  // console.log(tabIndex);
   return(
-    <>
-      <Tabs>
+    <Box alignItems='center' mt='3' p='3' bg={bg} style={{ width: '500px', height: '300px' }}>
+      <Center>
+        <Tabs onChange={(index) => setTabIndex(index)}>
         <TabList>
           <Tab>Pomodoro</Tab>
           <Tab>Short Break</Tab>
@@ -25,14 +26,30 @@ const MyTimer = () => {
           </TabPanel>
         </TabPanels>
       </Tabs>
-    </>
+      </Center>
+    </Box>
   )
 }
 
 const App = () => {
+  const colors = useColorModeValue(
+    ['red.200', 'teal.200', 'blue.200'],
+    ['red.900', 'teal.900', 'blue.900'],
+  )
+  const bgColors = useColorModeValue(
+    ['#F56565', '#38B2AC', '#4299E1'],
+    ['red.900', 'teal.900', 'blue.900'],
+  )
+  const [tabIndex, setTabIndex] = useState(0)
+  const bg = colors[tabIndex]
+  const bodyBg = bgColors[tabIndex]
+
   return (
-    <Container centerContent='true' bg='gray.200' p='3' alignItems='center' mt='3' style={{ width: '500px', height: '300px' }}>
-      <MyTimer />
+    <Container centerContent='true'>
+      <Helmet>
+        <style>{`body {background-color:${bodyBg};}`}</style>
+      </Helmet>
+      <MyTimer bg={bg} setTabIndex={setTabIndex}/>
     </Container>
   )
 }
